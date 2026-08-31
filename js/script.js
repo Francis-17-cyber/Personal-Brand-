cddce41de5bb2b1264f6d0aca4e3fea7e19c7fb6
Francis Shaba Miyoba <fomocopixel@gmail.com>
2026-08-31 18:06:52 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@
         const overlay  = document.getElementById('intro-overlay');
         const wordEl   = document.getElementById('intro-word');
         const nameEl   = document.getElementById('intro-name');
-        const fullName = "Francis SHABA Miyoba ItIS      -"
+        const fullName = "Francis SHABA Miyoba how is it fromt he other side-"
 
         let currentIndex = 0;
 
