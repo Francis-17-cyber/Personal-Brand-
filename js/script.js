67d1bd0895ebbcd866dd95cda0f7a0a0ee8e9204
Francis Shaba Miyoba <fomocopixel@gmail.com>
2026-09-18 18:01:47 -0500

    new

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@
         const overlay  = document.getElementById('intro-overlay');
         const wordEl   = document.getElementById('intro-word');
         const nameEl   = document.getElementById('intro-name');
-        const fullName = "Francis SHABA Miyoba how is the way we will do things from now and forever-"
+        const fullName = "Francis SHABA Miyoba the first-"
 
         let currentIndex = 0;
 
